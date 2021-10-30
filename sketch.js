dff634d7e2b67ecdfe50425e365a6a1b26de2b67
suparshav <73701670+suparshav@users.noreply.github.com>
2021-10-30 13:12:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var balloon,balloonImage1,balloonImage2,position;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonPosition=database.ref('balloon/height');
+  balloonPosition.on("value",readPosition,showError)
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    updateHeight(-10,0);
+    balloon.scale=balloon.scale -0.01;
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    updateHeight(10,0);
+    balloon.scale=balloon.scale -0.01;
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    updateHeight(0,10);
+    balloon.scale=balloon.scale -0.01;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    updateHeight(0,-10);
+    balloon.scale=balloon.scale -0.01;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function changePosition(x,y){
+  database.ref('balloon/height').set({
+      'x':position.x+x,
+      'y':position.y+y
+  })
+ // redball.x = redball.x + x;
+  //redball.y = redball.y + y;
+}
+function readPosition(data){
+  position=data.val();
+  balloon.x=balloon.x
+  balloon.y=balloon.y
+}
+
+
